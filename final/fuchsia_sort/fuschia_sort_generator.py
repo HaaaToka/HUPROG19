cc9de60ac491a9cfca5c8ac5ceed62557422a3e8
gokberksahin <b21627615@cs.hacettepe.edu.tr>
2019-03-01 20:55:18 +0300

    fuchsia sort

---
+++ final/fuchsia_sort/fuschia_sort_generator.py
@@ -0,0 +1,52 @@
+import random as rd
+from fuschia_sort_sol import fuchsia_sort
+
+
+def prng(n, exclude):
+    choices = [x for x in range(n) if x not in exclude]
+    return rd.choice(choices)
+
+input_sizes = [10, 100, 100, 100, 100, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 10**4, 10**4, 10**4, 10**4, 10**4, 10**4, 10**4, 10**4, 10**4, 10**5, 10**5, 10**5, 10**6, 10**6]
+k_sizes = [2, 17, 13, 14, 8, 56, 72, 99, 101, 342, 672, 240, 120, 78, 92, 133, 242, 562, 427, 356, 840, 257, 356, 216, 271, 302, 999, 666, 621, 987]
+for i in range(30):
+    fin = open("input" + str(i).zfill(2) + ".txt", "w")
+    fout = open("output" + str(i).zfill(2) + ".txt", "w")
+    k = k_sizes[i]
+    n = input_sizes[i]
+    len_of_sums = n - k + 1
+    sums = [rd.randint(k, 1000) for _ in range(len_of_sums)]
+    print(n, k, file=fin)
+    print(*sums, file=fin)
+    chosen = set()
+    known_nums = []
+    for _ in range(k - 1):
+        choice = prng(n, chosen)
+        ind = choice % k
+        for j in range(ind, n, k):
+            chosen.add(ind)
+        number = rd.randint(k, 1000)
+        print(choice, number, file=fin)
+        known_nums.append((choice, number))
+    out_arr = fuchsia_sort(n, k, sums, known_nums)
+    print(*out_arr, sep=" ", file=fout)
+
+            
+
+"""
+
+k=2 n=5 
+1 2 3 4 5 s = 4
+k=3 n=5
+1 2 3 4 5 s = 3
+k=4 n=5
+1 2 3 4 5 s = 2
+
+k=2 n=7
+1 2 3 4 5 6 7 s = 6
+k=3 n=7
+1 2 3 4 5 6 7 s = 5
+
+
+"""
+    
+
---
+++ final/fuchsia_sort/fuschia_sort_sol.py
@@ -0,0 +1,53 @@
+"""n, m = list(map(int, input().split()))
+sums = list(map(int, input().split()))
+
+known_nums = []
+for _ in range(m - 1):
+    ind, num = list(map(int, input().split()))
+    known_nums.append((ind, num))"""
+
+def fuchsia_sort(n, m, sums, known_nums):
+    seen_arr = [False] * n
+    ans_arr = [0] * n
+    for i in range(m - 1):
+        curr_ind_left = known_nums[i][0]
+        curr_num_left = known_nums[i][1]
+        curr_ind_right = known_nums[i][0]
+        curr_num_right = known_nums[i][1]
+        ans_arr[curr_ind_left - 1] = curr_num_left
+        seen_arr[curr_ind_left - 1] = True
+
+        while curr_ind_left - m >= 1:
+            curr_ind_left -= m
+            curr_num_left -= sums[curr_ind_left] - sums[curr_ind_left - 1]
+            ans_arr[curr_ind_left - 1] = curr_num_left
+            seen_arr[curr_ind_left - 1] = True
+
+        while curr_ind_right + m <= n:
+            curr_num_right += sums[curr_ind_right] - sums[curr_ind_right - 1] 
+            curr_ind_right += m
+            ans_arr[curr_ind_right - 1] = curr_num_right
+            seen_arr[curr_ind_right - 1] = True
+
+
+    ind = -1
+    for i in range(m):
+        if not seen_arr[i]:
+            ind = i + 1
+
+    assert(ind != -1)
+
+
+    curr_num = sums[0] - sum(ans_arr[0 : m])
+    ans_arr[ind - 1] = curr_num 
+
+
+    while ind + m <= n:
+        curr_num += sums[ind] - sums[ind - 1]
+        ind += m
+        ans_arr[ind - 1] = curr_num
+        
+
+    return ans_arr
+
+#print(fuchsia_sort(n, m, sums, known_nums))
