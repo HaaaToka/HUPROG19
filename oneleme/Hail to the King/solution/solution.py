4f34d2a5de755b4242eb4aad2ea9c0a1c6b1b50b
Onur Fatih DEMİR <31078178+FadeoN@users.noreply.github.com>
2019-03-18 22:22:47 +0300

    Add files via upload

--- oneleme/Hail to the King/solution/solution.py
+++ oneleme/Hail to the King/solution/solution.py
@@ -1,6 +1,44 @@
-import sys
-FILEDIR = ""
-sys.stdin = open(FILENDIR)
+def binary_search_upper(find, arr):
+    f = find
+    u = len(arr)-1
+    d = 0
+    while u >= d:
+        mid = d+(u-d)//2
+        if  arr[mid][0] > f:
+            u = mid-1
+        elif arr[mid][0] <= f:
+            d = mid +1
+        else:
+            break
+    return d+abs(u-d)//2
+
+def binary_search(find, arr):
+    f = find
+    u = len(arr)-1
+    d = 0
+    while u >= d:
+        mid = d+(u-d)//2
+        if  arr[mid][0] >= f:
+            u = mid-1
+        elif arr[mid][0] <f:
+            d = mid +1
+        else:
+            break
+    return d+abs(u-d)//2
+
+def binary_search_lower(find, arr):
+    f = find
+    u = len(arr)-1
+    d = 0
+    while u >= d:
+        mid = d+(u-d)//2
+        if  arr[mid][0] >= f:
+            u = mid-1
+        elif arr[mid][0] <f :
+            d = mid +1
+        else:
+            break
+    return d+abs(u-d)//2
 
 *_,k = list(map(int, input().split()))
 
@@ -20,9 +58,6 @@ for (n, d) in zip(scnd, scnd_degree):
 
 
 std = k
-
-
-start = time.time()
 total.sort(key=lambda x: x[0])
 
 
@@ -59,6 +94,10 @@ else:
     ref_l = l_idx
     ref_c = 0
     ref_med = median[0]
+    # print(total)
+    # print(ref_l, ref_r, "This")
+    # print(l_idx, r_idx)
+    #count first array
     for i in range(ref_l, ref_r + 1):
         if ref_med + k >= total[i][0] >= ref_med - k:
             if total[i][1] == 0:
@@ -74,6 +113,7 @@ else:
             cur_r = num + k
             cur_l = num - k
             while not (cur_r >= total[ref_l][0] >= cur_l):
+                # print(ref_l, len(total))
                 if total[ref_l][1] == 0:
                     ref_c -= 1
                 ref_l += 1
@@ -81,13 +121,15 @@ else:
                     left_flag = True
                     break
             while (cur_r >= total[ref_r][0] >= cur_l):
+                # print(ref_r , len(total))
                 if total[ref_r][1] == 0:
+                    # print(total[ref_r])
                     ref_c += 1
                 ref_r += 1
                 if ref_r > r_idx:
                     right_flag = True
                     break
-
+            # print(ref_c)
             if ref_c > max_c:
                 max_c = ref_c
                 ref_med = num
@@ -99,9 +141,3 @@ else:
     print(ref_med)
     print(max_c)
 
-
-
-
-
-
-#10.400695562362671
