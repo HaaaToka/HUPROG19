756560629aa07d7eeb661b0926757e1d01f1a992
gokberksahin <b21627615@cs.hacettepe.edu.tr>
2019-03-22 19:06:39 +0300

    fix bug

--- oneleme/zirnik/cozum.py
+++ oneleme/zirnik/cozum.py
@@ -5,27 +5,24 @@ pcs = []
 for _ in range(n):
     pcs.append(list(map(int, input().split())))
 
-
 fp_min = float("inf")
-fp_min_index = -1
+fp_min_index = -2
 for idx, pc in enumerate(pcs):
-    fare = pc[0]
-
-    if fare > x:
+    price = pc[0]
+    if price > x:
         continue
-
     minimum = min(pc[1:])
 
-    for i in range(1,len(pc)):
+    for i in range(1, len(pc)):
 
-        if abs(pc[i]-minimum)>k:
-            pc[i] = minimum+k
+        if abs(pc[i] - minimum) > k:
+            pc[i] = minimum + k
 
-    pow_sum = sum(pc) - fare
+    pow_sum = sum(pc) - price
     if pow_sum < p:
         continue
 
-    fp = fare/pow_sum
+    fp = price / pow_sum
     if fp_min > fp:
         fp_min = fp
         fp_min_index = idx
