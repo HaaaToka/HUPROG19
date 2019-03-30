ae29693fba30d5cd2b13c53a8364b5c7539e7807
tarikisildar <36731977+tarikisildar@users.noreply.github.com>
2019-03-30 22:52:59 +0300

    changeinsolution

--- final/Jenga/solution.py
+++ final/Jenga/solution.py
@@ -1,24 +1,20 @@
 def play(single,double):
-    if double == 1:
-        return "A"
-    elif (double%2) ^ (single%2) == 1:
-        return "A"
+    if double%2 == 0 and single %2 == 0:
+        return "Fatih"
     else:
-        return "B"
-fl = open(input(),"r")
-n = int(fl.readline())
-#n = int(input())
-single_mv = 0
-double_mv = 0
+        return "Onur"
+
+n = int(input())
+
 for i in range(n):
-    x,y,z = list(fl.readline())[:3]
-    #x,y,z = list(input())
-    count = [x,y,z].count("-")
-    if count == 3:
-        double_mv += 1
-    elif count == 2 and y != " ":
-        single_mv +=1
-print(play(single_mv,double_mv))
-
-
-    
+    single_mv = 0
+    double_mv = 0
+    k = int(input())
+    for e in range(k):
+        x,y,z = list(input())
+        count = [x,y,z].count("-")
+        if count == 3:
+            double_mv += 1
+        elif count == 2 and y != "*":
+            single_mv +=1
+    print(play(single_mv,double_mv))
