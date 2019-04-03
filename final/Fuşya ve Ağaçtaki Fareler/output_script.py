2ccf95179e146349a354a4c5448a41c87fe98c1d
gokberksahin <b21627615@cs.hacettepe.edu.tr>
2019-04-03 15:56:57 +0300

    metin fix

--- final/Fuşya ve Ağaçtaki Fareler/output_script.py
+++ final/Fuşya ve Ağaçtaki Fareler/output_script.py
@@ -5,9 +5,17 @@ run_str = "a.exe"
 os.system(compile_str)
 
 
-for i in range(20):
+"""for i in range(20):
     fin = open("input/input" + str(i).zfill(2) + ".txt", "r")
     fout = open("output/output" + str(i).zfill(2) + ".txt", "w")
     output = subprocess.check_output([run_str], stdin=fin)
-    print(*(str(output)[2:-1]).split("\\r\\n"), sep="\n", file=fout)
+    print(*(str(output)[2:-1]).split("\\r\\n"), sep="\n", file=fout)"""
+
+fin = open("ex_input.txt", "r")
+
+
+output = subprocess.check_output([run_str], stdin=fin)
+print(*(str(output)[2:-1]).split("\\r\\n"), sep="\n")
+
+
     
